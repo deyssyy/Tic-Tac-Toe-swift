0ae9285f8e2eef0658bb8a3729039130e5b4213a
Nikita Volya <Deysyy88@gmail.com>
2023-11-12 19:41:44 +0300

    Initial Commit

---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by nikita on 12.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
